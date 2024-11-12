ef867002df13c38402197c00baf1a5ef73f73579
in100tiva <projetoin100tiva@gmail.com>
2024-11-12 18:41:13 -0300

    teste

--- supabase.js
+++ supabase.js
@@ -1,14 +1,11 @@
 // supabase.js
 
-// Import the Supabase library and initialize the Supabase client
-const { createClient } = window.supabase;
-
-// Replace with your Supabase project's URL and anon key
+// Initialize the Supabase client
 const SUPABASE_URL = 'https://bciycuqjnvwpdcnzxbey.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjaXljdXFqbnZ3cGRjbnp4YmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NDU3OTYsImV4cCI6MjA0NzAyMTc5Nn0.BO9MkhfL0lqkMx-tMzz5j5_CbYTx97nQR33gzj7F0n0';
 
-// Create the Supabase client
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Create the Supabase client using the global Supabase library
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Function to verify if user exists in the database and authenticate
 async function verifyUser(username, password) {
